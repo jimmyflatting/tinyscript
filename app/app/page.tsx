804294769d3f35577c5f08e10c4bd8e7f5060d34
Jimmy Flatting <jimmy@Jimmys-Laptop.lan>
2024-08-22 19:20:43 +0200

    shadcn added

---
+++ app/app/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Link href="/">App</Link>
+    </div>
+  );
+}
