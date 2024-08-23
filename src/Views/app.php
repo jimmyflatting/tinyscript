f5255e678fff20de083e4eda1ad5a5b0fc49c5b6
Jimmy Flatting <jimmy.flatting@icloud.com>
2024-08-23 22:46:59 +0200

    silly

--- src/Views/app.php
+++ src/Views/app.php
@@ -1,11 +1,40 @@
 <?php
 
 get_template_part('Head');
+get_template_part('Navbar');
 ?>
 
 <body>
-  <h1>App</h1>
-  <p>This is the app page.</p>
+  <div class="app">
+    <h1>We are not ready for launch yet, silly!</h1>
+    <p>But you can still check out our pricing plans below.</p>
+    <?php get_template_part('Pricing'); ?>
+  </div>
+
+  <style>
+    body {
+      overflow: hidden;
+    }
+
+    .app {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+      text-align: center;
+    }
+
+    .app h1 {
+      font-size: 2rem;
+      margin-bottom: 10px;
+    }
+
+    .app p {
+      font-size: 1.2rem;
+      margin-bottom: 20px;
+    }
+  </style>
 </body>
 
 </html>
