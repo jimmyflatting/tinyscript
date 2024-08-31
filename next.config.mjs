cc0ce9009140a7de6d45d684972bcaaf5070d320
Jimmy Flatting <jimmy.flatting@icloud.com>
2024-08-31 22:13:10 +0200

    maybe???

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    DATABASE: process.env.DATABASE,
+    REPLICATE_API_TOKEN: process.env.REPLICATE_API_TOKEN,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    SYSTEM_PROMPT: process.env.SYSTEM_PROMPT,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  },
+};
 
 export default nextConfig;
