63c9ee7f57f994cfbc3825909563c1f8f129c4e4
Jimmy Flatting <jimmy.flatting@icloud.com>
2024-08-31 23:46:15 +0200

    docker setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    DATABASE: process.env.DATABASE,
-    REPLICATE_API_TOKEN: process.env.REPLICATE_API_TOKEN,
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    SYSTEM_PROMPT: process.env.SYSTEM_PROMPT,
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
