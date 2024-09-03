d81d996333f25a8e1e2148cb858dd154fe20f5ad
Jimmy Flatting <jimmy.flatting@icloud.com>
2024-09-03 11:09:49 +0200

    error logging

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,64 +1,43 @@
-import { NextResponse } from "next/server";
-import { getUser, createUser, updateUser } from "@/server/actions/user";
-import { User } from "@/lib/types";
-
-// export async function POST(req: Request, res: Response) {
-//     const { data } = await req.json();
-
-//     const user = await getUser(data.id);
-
-//     if (!user) {
-//         const userData: User = {
-//             _id: "",
-//             clerkId: data.id,
-//             full_name: data.first_name + " " + data.last_name,
-//             email: data.email_addresses[0].email_address,
-//             password: "",
-//             subscription_status: "trial",
-//             stripe: "",
-//             items: [],
-//         }
-
-//         const newUser = await createUser(userData);
-//         return NextResponse.json(newUser);
-//     }
-
-//     return NextResponse.json(user);
-// }
-
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+import { getUser, createUser, updateUser } from '@/server/actions/user';
+import { User } from '@/lib/types';
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { WebhookEvent } from '@clerk/nextjs/server';
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+    throw new Error(
+      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+    );
   }
 
+  console.log('Webhook triggered');
+
   // Get the headers
-  const headerPayload = headers()
-  const svix_id = headerPayload.get('svix-id')
-  const svix_timestamp = headerPayload.get('svix-timestamp')
-  const svix_signature = headerPayload.get('svix-signature')
+  const headerPayload = headers();
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+  const svix_signature = headerPayload.get('svix-signature');
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400,
-    })
+    });
   }
 
   // Get the body
-  const payload = await req.json()
-  const body = JSON.stringify(payload)
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET)
+  const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent
+  let evt: WebhookEvent;
 
   // Verify the payload with the headers
   try {
@@ -66,33 +45,33 @@ export async function POST(req: Request) {
       'svix-id': svix_id,
       'svix-timestamp': svix_timestamp,
       'svix-signature': svix_signature,
-    }) as WebhookEvent
+    }) as WebhookEvent;
   } catch (err) {
-    console.error('Error verifying webhook:', err)
+    console.error('Error verifying webhook:', err);
     return new Response('Error occured', {
       status: 400,
-    })
+    });
   }
 
   // Do something with the payload
-  // For this guide, you simply log the payload to the console
-  const { id } = evt.data
-  const eventType = evt.type
-//   console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-//   console.log('Webhook body:', body)
+  const { id } = evt.data;
+  const eventType = evt.type;
+
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log('Webhook body:', body);
 
   if (eventType === 'user.created') {
     const userData: Partial<User> = {};
 
     userData.clerkId = id as string;
-    userData.full_name = evt.data.first_name + " " + evt.data.last_name;
+    userData.full_name = evt.data.first_name + ' ' + evt.data.last_name;
     userData.email = evt.data.email_addresses[0].email_address;
     userData.password = evt.data.primary_email_address_id as string;
-    userData.subscription_status = "trial";
+    userData.subscription_status = 'trial';
     userData.items = [];
 
     await createUser(userData as User);
-    
+
     return NextResponse.json({ status: 200 });
   }
 
@@ -101,8 +80,11 @@ export async function POST(req: Request) {
 
     const userData: Partial<User> = {};
 
-    if (evt.data.first_name + " " + evt.data.last_name !== existingUser.full_name) {
-      userData.full_name = evt.data.first_name + " " + evt.data.last_name;
+    if (
+      evt.data.first_name + ' ' + evt.data.last_name !==
+      existingUser.full_name
+    ) {
+      userData.full_name = evt.data.first_name + ' ' + evt.data.last_name;
     }
 
     if (evt.data.email_addresses[0].email_address !== existingUser.email) {
@@ -119,5 +101,5 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ status: 200 });
   }
-  return new Response('', { status: 200 })
+  return new Response('', { status: 200 });
 }
