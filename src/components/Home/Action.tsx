c2ba034f108558d9be9f9592d6702974268b1462
Jimmy Flatting <jimmy.flatting@icloud.com>
2024-09-06 21:55:29 +0200

    fix?

--- src/components/Home/Action.tsx
+++ src/components/Home/Action.tsx
@@ -9,7 +9,7 @@ function Action() {
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-              It's just another LLM platform.
+              It&apos;s just another LLM platform.
             </h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               It&apos;s kinda good but try it out for yourself. - Any money made
