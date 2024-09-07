db9d4c322180ee9d2aba9d69e4d037d7173c1ff4
Jimmy Flatting <jimmy.flatting@icloud.com>
2024-09-07 12:46:51 +0200

    models tiers

--- src/server/actions/item.ts
+++ src/server/actions/item.ts
@@ -22,6 +22,10 @@ export async function createMessage(clerkId: string, content: string, conversati
     const user = await User.findOne({ clerkId });
     if (!user) throw new Error("User not found");
 
+    if (!user.available_tokens || user.available_tokens <= 0) {
+        throw new Error("User has no available tokens");
+    }
+
     let conversation;
     if (conversationId) {
         conversation = await ConversationModel.findById(conversationId);
@@ -38,19 +42,36 @@ export async function createMessage(clerkId: string, content: string, conversati
     const input = {
         prompt: content,
         max_new_tokens: 512,
-        prompt_template: "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+        prompt_template: "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+        system_prompt: process.env.SYSTEM_PROMPT
     };
 
     let aiResponse = '';
-    
-    for await (const event of replicate.stream("meta/meta-llama-3-8b-instruct", { input })) {
-        aiResponse += event;
-    };
+
+    if (user.subscription == "trial" || user.subscription == "starter") {
+        for await (const event of replicate.stream("meta/meta-llama-3-8b-instruct", { input })) {
+            aiResponse += event;
+        };
+    } else if (user.subscription == "plus") {
+        for await (const event of replicate.stream("meta/meta-llama-3-70b-instruct", { input })) {
+            aiResponse += event;
+        };
+    } else if (user.subscription == "pro") {
+        for await (const event of replicate.stream("meta/meta-llama-3.1-405b-instruct", { input })) {
+            aiResponse += event;
+        };
+    } else {
+        throw new Error("User is not subscribed to TinyScript.");
+    }
 
     conversation!.messages.push({ role: 'assistant', content: aiResponse });
 
     await conversation!.save();
-    
+
+    // Deduct tokens from user
+    user.available_tokens -= 1;
+    await user.save();
+
     return {
         conversationId: conversation!._id,
         userMessage: content,
